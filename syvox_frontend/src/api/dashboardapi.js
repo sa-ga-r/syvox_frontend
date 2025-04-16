ce4e9a5ffaf064afe77c2b1e1baeeb31eeadacf0
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-04-16 11:04:27 +0000

    update

--- syvox_frontend/src/api/dashboardapi.js
+++ syvox_frontend/src/api/dashboardapi.js
@@ -1,4 +1,4 @@
-const BASE_URL = "api/";
+const BASE_URL = "/api/";
 
 export const fetchTTSJobs = () => {
     return fetch(`${BASE_URL}TTS/tts_jobs/`)
@@ -6,6 +6,6 @@ export const fetchTTSJobs = () => {
 };
 
 export const fetchSTTJobs = () => {
-    return fetch(`${BASE_URL}STT/stt_jobs`)
+    return fetch(`${BASE_URL}STT/stt_jobs/`)
         .then(res => res.json());
 };
