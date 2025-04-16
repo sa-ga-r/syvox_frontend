1a5cfa318e64f43f532b007309c2151b2c8a648a
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-04-16 17:36:47 +0000

    update

--- syvox_frontend/vite.config.js
+++ syvox_frontend/vite.config.js
@@ -3,13 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://symmetrical-space-spoon-56qg5vjqr65cv5pw-8000.app.github.dev/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
 });
