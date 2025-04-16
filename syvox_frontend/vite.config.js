9386ea60bbf942d3b90d7dff2d47a4e83b6ecb7d
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-04-16 10:11:55 +0000

    update

--- syvox_frontend/vite.config.js
+++ syvox_frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://symmetrical-space-spoon-56qg5vjqr65cv5pw-8000.app.github.dev',
+        target: 'https://symmetrical-space-spoon-56qg5vjqr65cv5pw-8000.app.github.dev/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
