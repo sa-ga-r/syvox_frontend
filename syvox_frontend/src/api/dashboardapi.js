e41f7026488569fecb2792be6ce3cafe024cefcc
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2025-04-17 08:45:27 +0000

    update

--- syvox_frontend/src/api/dashboardapi.js
+++ syvox_frontend/src/api/dashboardapi.js
@@ -1,11 +1,23 @@
-const BASE_URL = "/api/";
+const BASE_URL = 'https://symmetrical-space-spoon-56qg5vjqr65cv5pw-8000.app.github.dev/';
 
 export const fetchTTSJobs = () => {
-    return fetch(`${BASE_URL}TTS/tts_jobs/`)
+    return fetch(`${BASE_URL}TTS/tts_jobs/`, {
+        method : "GET",
+        headers : {
+            "Accept" : "application/json",
+        },
+        mode : "cors"
+    })
         .then(res => res.json());
 };
 
 export const fetchSTTJobs = () => {
-    return fetch(`${BASE_URL}STT/stt_jobs/`)
+    return fetch(`${BASE_URL}STT/stt_jobs/`, {
+        method : "GET",
+        headers : {
+            "Accept" : "application/json",
+        },
+        mode : "cors"
+    })
         .then(res => res.json());
 };
